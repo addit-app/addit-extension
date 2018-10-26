9b618d8295ccbe1abfd75d0a003c78685a3975aa
CHANN <chann@chann.kr>
2018-10-27 01:40:30 +0900

    Init: Open in a new tab

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div className='App'>
+        <header className='App-header'>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href={window.location.href}
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Learn React
+            Open in new tab
           </a>
         </header>
       </div>
