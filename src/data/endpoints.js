d492909d3b7648cb62f254f0d3f933a7ca38f0fc
CHANN <chann@chann.kr>
2018-12-12 20:22:14 +0900

    Init: Default Endpoints

---
+++ src/data/endpoints.js
@@ -0,0 +1,24 @@
+export const mainNetEndpoints = [
+  'https://api.eosnewyork.io',
+]
+
+export const jungleNetEndpoints = [
+  'https://api.jungle.alohaeos.com',
+  'http://jungle2.cryptolions.io',
+  'http://peer.jungle.alohaeos.com',
+  'http://jungle2.cryptolions.io:443',
+  'http://jungle2.eosdac.io:8882',
+  'http://bp4-d3.eos42.io:8888',
+  'http://jungle.eosbcn.com:8080',
+  'http://jungle.eosmetal.io:18888',
+  'http://junglepeer.eossweden.se:443',
+  'http://18.223.252.15:8888',
+  'http://jungle.eosn.io:443',
+  'http://jungle-p2p.eosio.cr:443',
+  'http://145.239.133.201:8888',
+  'http://35.237.40.93:8888',
+  'http://88.99.193.44:8888',
+  'http://testnet.blockgenesys.com:443',
+]
+
+export default jungleNetEndpoints
